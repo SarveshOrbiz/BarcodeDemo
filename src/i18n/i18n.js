f64b203c4645cdb668516ea837d6d0b22de7c87a
SarveshOrbiz <142879328+SarveshOrbiz@users.noreply.github.com>
2023-08-22 14:14:08 +0530

    Added Detection

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -4,23 +4,22 @@ import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from 'react-i18next';
 
 i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
-    fallbackLng:"en",
+    fallbackLng: "en",
     /*
     debug:true, //Console logs the translation.json after every toggle
-
-    // Used to detect the user's preferred language from the browser.
-    detection:{
-        // The order of detection from sources.
-        order:['queryString','cookie'],
-        // Used to store the cookie.
-        cache:['cookie']
-    },
 
     // Used to determine whether special characters and HTML markups must be escaped or not.
      interpolation:{ 
         escapeValue:false, // By default React already does escaping.
     } 
     */
+    // Used to detect the user's preferred language from the browser.
+    detection: {
+        // The order of detection from sources.
+        order: ['queryString', 'cookie'],
+        // Used to store the cookie.
+        cache: ['cookie']
+    },
 })
 
 export default i18n;
